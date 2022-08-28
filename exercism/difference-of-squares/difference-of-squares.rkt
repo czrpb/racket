9d3970b325ea08f00de1745470ff83121c2eeba1
czrpb <czrpb@yahoo.com>
2022-08-28 12:13:50 -0700

    found and using a "functional" library to do currying and composition

--- exercism/difference-of-squares/difference-of-squares.rkt
+++ exercism/difference-of-squares/difference-of-squares.rkt
@@ -1,20 +1,18 @@
 #lang racket
 
+(require relation/function)
+
 (provide sum-of-squares square-of-sum difference)
 
 (define (sum-of-squares n)
-  (let* ([ns (range 1 (add1 n))]
-        [ns_squared (map sqr ns)]
-        [ns_squared_summed (foldl + 0 ns_squared)])
-    ns_squared_summed
-    ))
+  (for/sum ([ns (range 1 (add1 n))]) (sqr ns))
+  )
 
 
 (define (square-of-sum n)
-  (let* ([ns (range 1 (add1 n))]
-        [ns_summed (foldl + 0 ns)]
-        [ns_summed_squared (sqr ns_summed)])
-    ns_summed_squared
-    ))
+  (let* ([range1 (curry range 1)]
+         [sum (curry apply +)]
+         [doit (f #:thread? #t add1 range1 sum sqr)])
+    (doit n)))
 
 (define (difference n) (- (square-of-sum n) (sum-of-squares n)))
