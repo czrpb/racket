3f822b231d2bef9a7ae15414161724bfad4fcb69
czrpb <czrpb@yahoo.com>
2022-08-30 21:54:27 -0700

    updates

--- exercism/armstrong-numbers/armstrong-numbers.rkt
+++ exercism/armstrong-numbers/armstrong-numbers.rkt
@@ -3,18 +3,11 @@
 (provide armstrong-number?)
 
 (define (armstrong-number? n)
-  (define/match (f n digits_count)
-    [(0 _) digits_count]
-    [(_ _) (let (
-                 [q (quotient n 10)]
-                 [r (remainder n 10)]
-                 [digits (car digits_count)]
-                 [count (cdr digits_count)])
-             (f q (cons (cons r digits) (add1 count)))
-             )])
   (let* (
-         [digits_count (f n (cons '() 0))]
-         [digits (car digits_count)]
-         [count (cdr digits_count)])
+         [char->int (compose string->number string)]
+         [number->list (compose string->list number->string)]
+         [digits-str (number->list n)]
+         [digits (map char->int digits-str)]
+         [count (length digits)])
     (= n (for/sum ([d digits]) (expt d count))))
   )
