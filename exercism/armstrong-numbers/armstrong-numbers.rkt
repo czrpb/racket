ee8fbe841fad74a12940349858d6c39181bb94e2
czrpb <czrpb@yahoo.com>
2022-08-30 21:33:57 -0700

    exercism armstrong number solved

--- exercism/armstrong-numbers/armstrong-numbers.rkt
+++ exercism/armstrong-numbers/armstrong-numbers.rkt
@@ -3,4 +3,18 @@
 (provide armstrong-number?)
 
 (define (armstrong-number? n)
-  "not implemented")
+  (define/match (f n digits_count)
+    [(0 _) digits_count]
+    [(_ _) (let (
+                 [q (quotient n 10)]
+                 [r (remainder n 10)]
+                 [digits (car digits_count)]
+                 [count (cdr digits_count)])
+             (f q (cons (cons r digits) (add1 count)))
+             )])
+  (let* (
+         [digits_count (f n (cons '() 0))]
+         [digits (car digits_count)]
+         [count (cdr digits_count)])
+    (= n (for/sum ([d digits]) (expt d count))))
+  )
