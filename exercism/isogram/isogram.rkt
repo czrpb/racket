3f0b10432a1274969d3edd4ddf62fc0e4a3ed3c8
czrpb <czrpb@yahoo.com>
2022-08-31 14:06:16 -0700

    more functions

--- exercism/isogram/isogram.rkt
+++ exercism/isogram/isogram.rkt
@@ -3,4 +3,11 @@
 (provide isogram?)
 
 (define (isogram? s)
-  (error "not implemented"))
+  (let* (
+         [string->downlist (compose string->list string-downcase)]
+         [string->alphas (lambda (s) (filter char-alphabetic? (string->downlist s)))]
+         [s-alphas-as-list (string->alphas s)]
+         [s-alphas-as-set (list->set s-alphas-as-list)])
+
+    (= (length s-alphas-as-list) (set-count s-alphas-as-set))
+    ))
