aab06eb1ced305b64027d993d07dcfedf7831355
czrpb <czrpb@yahoo.com>
2022-09-01 09:30:59 -0700

    updated

--- exercism/raindrops/raindrops.rkt
+++ exercism/raindrops/raindrops.rkt
@@ -4,15 +4,15 @@
 
 (define (convert n)
   (let* (
-         [mod (lambda (n) (lambda (x) (= (remainder x n) 0)))]
-         [mods (list (mod 3) (mod 5) (mod 7))]
+         [divby? (lambda (n) (lambda (x) (= (remainder x n) 0)))]
+         [divby357 (list (divby? 3) (divby? 5) (divby? 7))]
          [sounds '("Pling" "Plang" "Plong")]
          )
     (match
         (for/fold
          ([r ""])
-         ([m mods] [s sounds])
-          (string-append r (if (m n) s "")))
+         ([divby? divby357] [s sounds])
+          (string-append r (if (divby? n) s "")))
       ["" (number->string n)]
       [x x]
       )))
