7d42084a7f3f6c36313f8788035347fbd856471d
czrpb <czrpb@yahoo.com>
2022-09-01 06:56:51 -0700

    implemented

--- exercism/raindrops/raindrops.rkt
+++ exercism/raindrops/raindrops.rkt
@@ -1,3 +1,19 @@
 #lang racket
 
 (provide convert)
+
+(define (convert n)
+  (let* (
+         [mod (lambda (n) (lambda (x) (= (remainder x n) 0)))]
+         [mods (list (mod 3) (mod 5) (mod 7))]
+         [sounds '("Pling" "Plang" "Plong")]
+         )
+    (match
+        (for/fold
+         ([r ""])
+         ([m mods] [s sounds])
+          (string-append r (if (m n) s "")))
+      ["" (number->string n)]
+      [x x]
+      )))
+      
