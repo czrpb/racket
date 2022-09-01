39b8f9e1a3b43b75c5b71c299754b1dd336b1dc3
czrpb <czrpb@yahoo.com>
2022-08-31 18:07:47 -0700

    implemented

--- exercism/acronym/acronym.rkt
+++ exercism/acronym/acronym.rkt
@@ -1,3 +1,14 @@
 #lang racket
 
 (provide acronym)
+
+(define (acronym sentence)
+  (let (
+        [first-char (lambda (s) (string-ref s 0))]
+        [list->string-upcase (compose string-upcase list->string)])
+    (let* (
+           [words (string-split sentence #px"[\\s_-]+")]
+           [first-chars (map first-char words)]
+           )
+      (list->string-upcase first-chars)
+      )))
