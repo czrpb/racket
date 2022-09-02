f8f7cbd91eecfd412d78c50905af22d26fbdc119
czrpb <czrpb@yahoo.com>
2022-09-01 20:23:44 -0700

    improved

--- exercism/anagram/anagram.rkt
+++ exercism/anagram/anagram.rkt
@@ -1,3 +1,19 @@
 #lang racket
 
 (provide anagrams-for)
+
+(define (anagrams-for word possibles)
+  (let* ([string-downcase->list (compose string->list string-downcase)]
+         [string-sort (lambda (s) (sort (string-downcase->list s) char-ci<?))]
+         [word-chars (string-sort word)])
+    (filter
+     (lambda (possible)
+       (and (not (equal? word possible))
+            (= (string-length word) (string-length possible))
+            (let ([possible-chars (string-sort possible)])
+              (for/and ([w word-chars] [p possible-chars])
+                (equal? w p)))))
+     possibles
+     )
+    )
+  )
