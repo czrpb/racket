678f47f43107582a91425d330f47fd8536163475
czrpb <czrpb@yahoo.com>
2022-09-02 07:43:31 -0700

    implemented

--- exercism/leap/leap.rkt
+++ exercism/leap/leap.rkt
@@ -1,3 +1,17 @@
 #lang racket
 
 (provide leap-year?)
+
+(define (leap-year? year)
+  (let* (
+         [div-by (lambda (d) (lambda (y) (= (remainder y d) 0)))]
+         [div-by-400 (div-by 400)]
+         [div-by-100 (div-by 100)]
+         [div-by-4 (div-by 4)]
+         )
+    (match year
+      [(app div-by-400 #t) #t]
+      [(app div-by-100 #t) #f]
+      [(app div-by-4 #t) #t]
+      [else #f]
+      )))
