5e5223a1d4d740ded42ba9b33183d35bb9194fd9
czrpb <czrpb@yahoo.com>
2022-09-02 13:40:37 -0700

    implemented

--- exercism/allergies/allergies.rkt
+++ exercism/allergies/allergies.rkt
@@ -2,6 +2,27 @@
 
 (provide list-allergies allergic-to?)
 
-(define (list-allergies score))
+(define allergines
+  (hash
+   "eggs" 1
+   "peanuts" 2
+   "shellfish" 4
+   "strawberries" 8
+   "tomatoes" 16
+   "chocolate" 32
+   "pollen" 64
+   "cats" 128
+   ))
 
-(define (allergic-to? str))
+(define (allergic-to? str score)
+  (not
+   (= (bitwise-and (hash-ref allergines str) score)
+      0)))
+
+(define (list-allergies score)
+  (let (
+        [allergic-to-score?
+         (lambda (allergin) (allergic-to? allergin score))
+         ])
+    (filter allergic-to-score? (hash-keys allergines))
+    ))
