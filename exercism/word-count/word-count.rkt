b7c97ec73acd1d85f901cb4cdcadb4d9c45886ff
czrpb <czrpb@yahoo.com>
2022-09-02 19:17:22 -0700

    implemented

--- exercism/word-count/word-count.rkt
+++ exercism/word-count/word-count.rkt
@@ -2,3 +2,12 @@
 
 (provide word-count)
 
+(define (word-count phrase)
+  (let*([string-or-false (match-lambda ["" #f] [s s])]
+        [hash-update!-count
+         (lambda (k h) (hash-update h k add1 0))]
+        [string-trim-puncuation (lambda (s) (string-trim s #px"[-_'!@$%^&]+"))])
+    (foldl hash-update!-count (hash)
+           (filter-map (compose string-or-false string-trim-puncuation string-downcase)
+                       (string-split phrase #px"[\n\\s.,\":]+")))
+    ))
