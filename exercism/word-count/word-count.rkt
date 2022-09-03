11fb32734e0a4d90b02566b458655319327e245e
czrpb <czrpb@yahoo.com>
2022-09-02 20:48:44 -0700

    improvements

--- exercism/word-count/word-count.rkt
+++ exercism/word-count/word-count.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+(require math/statistics)
+
 (provide word-count)
 
 (define (word-count phrase)
@@ -8,9 +10,7 @@
          [phrase (string-replace phrase punc-regex " ")]
          [phrase (string-downcase phrase)]
          [phrase-as-list (string-split phrase whitespace-regex)]
-         [phrase-as-list (map (lambda (s) (string-trim s "'")) phrase-as-list)]
-         [hash-update!-count
-          (lambda (k h) (hash-update h k add1 0))])
-    (foldl hash-update!-count (hash) phrase-as-list)
+         [phrase-as-list (map (lambda (s) (string-trim s "'")) phrase-as-list)])
+    (samples->hash phrase-as-list)
     )
   )
