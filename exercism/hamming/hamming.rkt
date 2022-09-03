86b00cdba94e53d979d8890bb2f4281b3d5fe53f
czrpb <czrpb@yahoo.com>
2022-09-03 14:05:37 -0700

    improvements

--- exercism/hamming/hamming.rkt
+++ exercism/hamming/hamming.rkt
@@ -8,3 +8,10 @@
         (if (equal? s1 s2) 0 1))
       (error "Strands are of different length")
       ))
+
+;Fantastic solution from the community:
+
+;  (define (hamming-distance str1 str2)
+;    (count (negate equal?)
+;           (string->list str1)
+;           (string->list str2)))
