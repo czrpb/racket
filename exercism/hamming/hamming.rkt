277987885cc3b516736cd4760e41d24e8a0d695d
czrpb <czrpb@yahoo.com>
2022-09-03 14:00:20 -0700

    implemented

--- exercism/hamming/hamming.rkt
+++ exercism/hamming/hamming.rkt
@@ -1,3 +1,10 @@
 #lang racket
 
 (provide hamming-distance)
+
+(define (hamming-distance strand1 strand2)
+  (if (= (string-length strand1)(string-length strand2))
+      (for/sum ([s1 strand1] [s2 strand2])
+        (if (equal? s1 s2) 0 1))
+      (error "Strands are of different length")
+      ))
