e871869fb83985c5db2e66250cbca104ac47d7e1
czrpb <czrpb@yahoo.com>
2022-09-04 15:17:38 -0700

    improved based on my elixir solution

--- exercism/etl/etl.rkt
+++ exercism/etl/etl.rkt
@@ -1,25 +1,15 @@
 #lang racket
 
-(require racket/hash)
-
 (provide etl)
 
 (define (etl old)
-  (let* (
-         [not-empty? (negate empty?)]
-         [letter-hash (lambda (score letters)
-                        (if (nonnegative-integer? score)
-                            (for/hash ([letter letters])
-                              (values (string-downcase letter) score))
-                            (raise (exn:fail:contract "Invalid score"))
-                            ))]
-         [converted-hash (hash-map old letter-hash)]
-         )
-    (if (not-empty? converted-hash)
-        (apply hash-union converted-hash)
-        (hash))
-    ))
-
+  (for*/hash ([(score letters) old]
+              #:do [(or (nonnegative-integer? score)
+                        (raise (exn:fail:contract "Invalid score" (current-continuation-marks))))]
+              [letter letters])
+    (values (string-downcase letter) score)
+    )
+  )
 
 ; Very cool community implementation:
 
