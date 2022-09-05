f52acef6eb3d314fefb0bf113dc454e7a58c765e
czrpb <czrpb@yahoo.com>
2022-09-05 10:22:45 -0700

    implemented

--- exercism/collatz-conjecture/collatz-conjecture.rkt
+++ exercism/collatz-conjecture/collatz-conjecture.rkt
@@ -2,5 +2,15 @@
 
 (provide collatz)
 
-  
-
+(define (collatz n)
+  (if (or ((negate natural?) n) (zero? n))
+      (error "Invalid input")
+      (letrec ([n/2 (lambda (n) (/ n 2))]
+               [3n+1 (lambda (n) (add1 (* n 3)))]
+               [collatz (lambda (n c)
+                          (cond
+                            [(= n 1) c]
+                            [(even? n) (collatz (n/2 n) (add1 c))]
+                            [else (collatz (3n+1 n) (add1 c))]))])
+        (collatz n 0)
+        )))
