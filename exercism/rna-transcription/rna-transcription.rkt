95a96dd90aacaa21739f0297f0dbf41d91619ee9
czrpb <czrpb@yahoo.com>
2022-09-05 19:50:36 -0700

    other implementation

--- exercism/rna-transcription/rna-transcription.rkt
+++ exercism/rna-transcription/rna-transcription.rkt
@@ -6,4 +6,7 @@
   (let ([dna-to-rna (hash #\G #\C #\C #\G #\T #\A #\A #\U)])
     (apply string
            (for/list ([nuc dna]) (hash-ref dna-to-rna nuc)))
+    ; (apply string (map
+    ;                (match-lambda [#\G #\C] [#\C #\G] [#\T #\A] [#\A #\U])
+    ;                (string->list dna)))
     ))
