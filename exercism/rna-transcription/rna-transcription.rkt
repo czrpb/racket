842de1f94914be5c713c3108e5d3342f718cf281
czrpb <czrpb@yahoo.com>
2022-09-05 19:45:54 -0700

    implemented

--- exercism/rna-transcription/rna-transcription.rkt
+++ exercism/rna-transcription/rna-transcription.rkt
@@ -1,3 +1,9 @@
 #lang racket
 
 (provide to-rna)
+
+(define (to-rna dna)
+  (let ([dna-to-rna (hash #\G #\C #\C #\G #\T #\A #\A #\U)])
+    (apply string
+           (for/list ([nuc dna]) (hash-ref dna-to-rna nuc)))
+    ))
