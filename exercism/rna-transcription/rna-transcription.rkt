19c8024708da793bd43fac5e3c60743ea2f97eeb
czrpb <czrpb@yahoo.com>
2022-09-05 19:53:34 -0700

    improved

--- exercism/rna-transcription/rna-transcription.rkt
+++ exercism/rna-transcription/rna-transcription.rkt
@@ -1,12 +1,15 @@
 #lang racket
 
+(require srfi/13)
+
 (provide to-rna)
 
 (define (to-rna dna)
   (let ([dna-to-rna (hash #\G #\C #\C #\G #\T #\A #\A #\U)])
-    (apply string
-           (for/list ([nuc dna]) (hash-ref dna-to-rna nuc)))
+    ; (apply string
+    ;        (for/list ([nuc dna]) (hash-ref dna-to-rna nuc)))
     ; (apply string (map
     ;                (match-lambda [#\G #\C] [#\C #\G] [#\T #\A] [#\A #\U])
     ;                (string->list dna)))
+    (string-map (match-lambda [#\G #\C] [#\C #\G] [#\T #\A] [#\A #\U]) dna)
     ))
