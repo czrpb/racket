e72e7632e06e44731dd097c7e02d1ee44224a174
czrpb <czrpb@yahoo.com>
2022-09-06 10:01:52 -0700

    implemented

--- exercism/gigasecond/gigasecond.rkt
+++ exercism/gigasecond/gigasecond.rkt
@@ -4,3 +4,9 @@
 
 (require racket/date)
 
+(define (add-gigasecond moment)
+  (let* ([gig 1000000000]
+         [addgig (lambda (seconds) (+ seconds gig))]
+         [future-gig (compose seconds->date addgig date->seconds)])
+    (future-gig moment)
+    ))
