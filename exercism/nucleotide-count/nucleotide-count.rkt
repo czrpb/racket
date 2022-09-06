db619bae5f70e7cae257e5c7f4a886c6eb5f7093
czrpb <czrpb@yahoo.com>
2022-09-05 19:24:10 -0700

    implemented

--- exercism/nucleotide-count/nucleotide-count.rkt
+++ exercism/nucleotide-count/nucleotide-count.rkt
@@ -1,3 +1,19 @@
 #lang racket
 
+(require math/statistics racket/hash)
+
 (provide nucleotide-counts)
+
+(define (nucleotide-counts sequence)
+  (let* ([all-nucleotides (hash #\A 0 #\C 0 #\G 0 #\T 0)]
+         [sequence (for/list ([s sequence])
+                     (if [hash-has-key? all-nucleotides s] s (error "Invalid nucleotide")))])
+    (sort
+     (hash->list
+      [hash-union (hash) ; bit of a hack since hash-union requires JUST the 1st arg to be immutable
+                         ; thx to SORAWEE on discord: https://discord.com/channels/571040468092321801/667522224823205889/1016450844360716328
+                  (samples->hash sequence)
+                  all-nucleotides
+                  #:combine/key (lambda (k a b) a)])
+     (lambda (p1 p2) (char-ci<? p1 p2)) #:key car
+     )))
