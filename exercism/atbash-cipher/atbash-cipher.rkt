939e76df0b75d4ed651f7dda29bffc8133fa7f31
czrpb <czrpb@yahoo.com>
2022-09-07 12:11:58 -0700

    using ~> (aka pipe) from threading

--- exercism/atbash-cipher/atbash-cipher.rkt
+++ exercism/atbash-cipher/atbash-cipher.rkt
@@ -1,5 +1,6 @@
 #lang racket
 
+(require threading)
 (require (only-in srfi/13 string-map string-reverse string-take string-drop))
 
 (provide encode decode)
@@ -14,23 +15,22 @@
   (for/hash ([a from] [b to]) (values a b)))
 
 (define (process translation-hash msg bin?)
-  (list->string
-   (reverse
-    (car
-     (foldl (match-lambda**
-             [(char (cons result counter))
-              (let* ([translated-char (hash-ref translation-hash char #f)]
-                     [result-with?-space
-                      (if (and bin? translated-char (zero? counter))
-                          (cons #\  result) result)]
-                     [counter+1 (modulo (add1 counter) 5)])
-                (if translated-char
-                    (cons (cons translated-char result-with?-space) counter+1)
-                    (cons result counter)
-                    ))])
-            (cons '() 5)
-            (string->list msg))
-     ))))
+  (~>
+   (foldl (match-lambda**
+           [(char (cons result counter))
+            (let* ([translated-char (hash-ref translation-hash char #f)]
+                   [result-with?-space
+                    (if (and bin? translated-char (zero? counter))
+                        (cons #\  result) result)]
+                   [counter+1 (modulo (add1 counter) 5)])
+              (if translated-char
+                  (cons (cons translated-char result-with?-space) counter+1)
+                  (cons result counter)
+                  ))])
+          (cons '() 5)
+          (string->list msg))
+   car reverse list->string)
+  )
 
 (define (encode msg)
   (let ([translation-hash (for/hash-from->to alphabet tebahpla)])
