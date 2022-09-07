7d8cff72729fad7e94b6ea57d190a08d0f195b0d
czrpb <czrpb@yahoo.com>
2022-09-06 20:09:04 -0700

    improved

--- exercism/atbash-cipher/atbash-cipher.rkt
+++ exercism/atbash-cipher/atbash-cipher.rkt
@@ -10,41 +10,30 @@
 (define alphabet (string-append letters numbers))
 (define tebahpla (string-append (string-reverse letters) numbers))
 
-(define to-from (for/hash ([a alphabet] [z tebahpla])
-                  (values a z)))
-(define to->from (lambda (str)
-                   (string-map (lambda (c)
-                                 (hash-ref to-from c))
-                               str)))
+(define (for/hash-from->to from to)
+  (for/hash ([a from] [b to]) (values a b)))
 
-(define from-to (for/hash ([a alphabet] [z tebahpla])
-                  (values z a)))
-(define from->to (lambda (str)
-                   (string-map (lambda (c)
-                                 (hash-ref from-to c))
-                               str)))
+(define (translate trans-hash str)
+    (string-map (lambda (c) [hash-ref trans-hash c]) str))
 
-(define (process m direction)
-  (letrec ([alnum #px"[^a-z0-9]"]
-           [string-remove-non-alnum (lambda (s)
-                                      (string-replace s alnum ""))]
-           [clean (compose string-remove-non-alnum string-downcase)]
+(define (process msg from to)
+  (letrec ([~alnum #px"[^a-z0-9]"]
+           [string-remove-~alnum (lambda (s) [string-replace s ~alnum ""])]
+           [clean (compose string-remove-~alnum string-downcase)]
            )
-    (direction (clean m))
+    (translate (for/hash-from->to from to) (clean msg))
     ))
 
 (define (encode m)
-  (letrec ([string-length-remaining-or-5 (lambda (s) (if (< (string-length s) 5)
-                                                         (string-length s) 5))]
+  (letrec ([first-^5 (lambda (s) [if (< (string-length s) 5) (string-length s) 5])]
+           [string-bin (lambda (s) [let ([avail (first-^5 s)])
+                                     (cons (string-drop s avail) (string-take s avail))])]
            [bin (match-lambda** [("" binned) binned]
-                                [(s binned) (bin (string-drop s (string-length-remaining-or-5 s))
-                                                 (string-append binned " "
-                                                                (string-take s
-                                                                             (string-length-remaining-or-5 s))))])]
+                                [(s binned) (match-let ([(cons a b) (string-bin s)])
+                                              (bin a (string-append binned " " b)))])]
            )
-    (string-trim (bin (process m to->from) ""))
+    (string-trim (bin (process m alphabet tebahpla) ""))
     ))
 
 (define (decode m)
-  (process m from->to)
-  )
+  (process m tebahpla alphabet))
