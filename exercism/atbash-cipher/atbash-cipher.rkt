19bd649b7cab95c35a8b2a5ef405e23dea0795a8
czrpb <czrpb@yahoo.com>
2022-09-07 16:05:59 -0700

    improved

--- exercism/atbash-cipher/atbash-cipher.rkt
+++ exercism/atbash-cipher/atbash-cipher.rkt
@@ -16,9 +16,9 @@
 
 (define (process translation-hash msg bin?)
   (~> ; like Elixir's |>
-      ; 1st expression is sent to the 2nd argument which is a function
-      ; whose output is sent to the next function and onward
-      ; https://docs.racket-lang.org/threading/index.html#%28form._%28%28lib._threading%2Fmain..rkt%29._~7e~3e%29%29|
+   ; 1st expression is sent to the 2nd argument which is a function
+   ; whose output is sent to the next function and onward
+   ; https://docs.racket-lang.org/threading/index.html#%28form._%28%28lib._threading%2Fmain..rkt%29._~7e~3e%29%29|
    (foldl (match-lambda**
            [(char (cons result counter))
             (let* ([translated-char (hash-ref translation-hash char #f)]
@@ -42,8 +42,7 @@
       ))
 
 (define (decode msg)
-  (~>
-   msg
-   string-downcase
-   (process cipher _ #f)
-   ))
+  (~> msg
+      string-downcase
+      (process cipher _ #f)
+      ))
