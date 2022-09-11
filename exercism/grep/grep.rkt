88e4ba113e2dfd8e6a36bf1a5f048f4b75e505be
czrpb <czrpb00@gmail.com>
2022-09-11 16:57:29 -0700

    updates

--- exercism/grep/grep.rkt
+++ exercism/grep/grep.rkt
@@ -1,13 +1,25 @@
 #lang racket
 
+(require threading)
+
 (provide grep)
 
 (define (grep flags str file )
-  (let ([nats (in-naturals)]
-        [lines (in-lines (open-input-file file #:mode 'text))]
-        [contains (lambda (line) (string-contains? line str))])
-    (sequence->list
-     (sequence-filter contains lines))
+  (let* ([join-with-lf (lambda (l) (string-join l "\n"))]
+         [format-result (compose join-with-lf reverse)]
+         [nats (in-naturals)]
+         [lines (in-lines (open-input-file file #:mode 'text))]
+         [contains (lambda (line) (string-contains? line str))])
+    (format-result
+     (for/fold ([result '()]) ([nat nats] [line lines])
+       (if (contains line)
+           (let* ([add-line-num (member "-n" flags)]
+                  [line (if add-line-num
+                            (~a nat ":" line) line)])
+             (cons line result))
+           result)
+       ))
+
     ))
 
-(grep '() "and" "iliad.txt")
+(grep '("-n") "and" "iliad.txt")
