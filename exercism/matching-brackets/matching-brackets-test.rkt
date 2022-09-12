c37845998aa0a9d82f689b928948425f937a314d
czrpb <czrpb00@gmail.com>
2022-09-12 13:06:44 -0700

    updates

--- exercism/matching-brackets/matching-brackets-test.rkt
+++ exercism/matching-brackets/matching-brackets-test.rkt
@@ -10,43 +10,43 @@
      "matching brackets tests"
 
      ; Simple passing tests
-     (test-eqv? "empty is balenced" (balanced? "") #t)
-     (test-eqv? "'[]' is balenced" (balanced? "[]") #t)
-     (test-eqv? "'{}' is balenced" (balanced? "{}") #t)
-     (test-eqv? "'{ }' is balenced" (balanced? "{ }") #t)
-     (test-eqv? "'()' is balenced" (balanced? "()") #t)
+     (test-eqv? "empty is balanced" (balanced? "") #t)
+     (test-eqv? "'[]' is balanced" (balanced? "[]") #t)
+     (test-eqv? "'{}' is balanced" (balanced? "{}") #t)
+     (test-eqv? "'{ }' is balanced" (balanced? "{ }") #t)
+     (test-eqv? "'()' is balanced" (balanced? "()") #t)
 
      ; Complicated passing tests
-     (test-eqv? "'()[]{}' is balenced" (balanced? "()[]{}") #t)
-     (test-eqv? "'([{}])' is balenced" (balanced? "([{}])") #t)
-     (test-eqv? "'([{}({}[])])' is balenced" (balanced? "([{}({}[])])") #t)
+     (test-eqv? "'()[]{}' is balanced" (balanced? "()[]{}") #t)
+     (test-eqv? "'([{}])' is balanced" (balanced? "([{}])") #t)
+     (test-eqv? "'([{}({}[])])' is balanced" (balanced? "([{}({}[])])") #t)
 
-     ; Additional characters but still balenced
-     (test-eqv? "'123' is balenced" (balanced? "123") #t)
-     (test-eqv? "'[1]' is balenced" (balanced? "[1]") #t)
-     (test-eqv? "'(((185 + 223.85) * 15) - 543)/2' is balenced" (balanced? "(((185 + 223.85) * 15) - 543)/2") #t)
-     (test-eqv? "'a{b}c(d)e[f]g' is balenced" (balanced? "a{b}c(d)e[f]g") #t)
-     (test-eqv? "'a{b(c[de]f)g}h' is balenced" (balanced? "a{b(c[de]f)g}h") #t)
+     ; Additional characters but still balanced
+     (test-eqv? "'123' is balanced" (balanced? "123") #t)
+     (test-eqv? "'[1]' is balanced" (balanced? "[1]") #t)
+     (test-eqv? "'(((185 + 223.85) * 15) - 543)/2' is balanced" (balanced? "(((185 + 223.85) * 15) - 543)/2") #t)
+     (test-eqv? "'a{b}c(d)e[f]g' is balanced" (balanced? "a{b}c(d)e[f]g") #t)
+     (test-eqv? "'a{b(c[de]f)g}h' is balanced" (balanced? "a{b(c[de]f)g}h") #t)
 
      ; Simple failing tests
-     (test-eqv? "'][' is UN-balenced" (balanced? "][") #f)
-     (test-eqv? "')(' is UN-balenced" (balanced? ")(") #f)
-     (test-eqv? "'}{' is UN-balenced" (balanced? "}{") #f)
-     (test-eqv? "'(]' is UN-balenced" (balanced? "(]") #f)
-     (test-eqv? "']]' is UN-balenced" (balanced? "][") #f)
-     (test-eqv? "'((' is UN-balenced" (balanced? "((") #f)
+     (test-eqv? "'][' is UN-balanced" (balanced? "][") #f)
+     (test-eqv? "')(' is UN-balanced" (balanced? ")(") #f)
+     (test-eqv? "'}{' is UN-balanced" (balanced? "}{") #f)
+     (test-eqv? "'(]' is UN-balanced" (balanced? "(]") #f)
+     (test-eqv? "']]' is UN-balanced" (balanced? "][") #f)
+     (test-eqv? "'((' is UN-balanced" (balanced? "((") #f)
 
      ; Complicated failing tests
-     (test-eqv? "'{}[' is UN-balenced" (balanced? "{}[") #f)
-     (test-eqv? "'[}]' is UN-balenced" (balanced? "[}]") #f)
-     (test-eqv? "'{[}' is UN-balenced" (balanced? "{[}") #f)
-     (test-eqv? "'{[])' is UN-balenced" (balanced? "{[])") #f)
-     (test-eqv? "'{}[()' is UN-balenced" (balanced? "{}[()") #f)
-     (test-eqv? "'{[)][]}' is UN-balenced" (balanced? "{[)][]}") #f)
+     (test-eqv? "'{}[' is UN-balanced" (balanced? "{}[") #f)
+     (test-eqv? "'[}]' is UN-balanced" (balanced? "[}]") #f)
+     (test-eqv? "'{[}' is UN-balanced" (balanced? "{[}") #f)
+     (test-eqv? "'{[])' is UN-balanced" (balanced? "{[])") #f)
+     (test-eqv? "'{}[()' is UN-balanced" (balanced? "{}[()") #f)
+     (test-eqv? "'{[)][]}' is UN-balanced" (balanced? "{[)][]}") #f)
 
-     ; Additional characters but unbalenced
-     (test-eqv? "'(1]' is UN-balenced" (balanced? "(1]") #f)
-     (test-eqv? "'{}[1' is UN-balenced" (balanced? "{}[1") #f)
+     ; Additional characters but unbalanced
+     (test-eqv? "'(1]' is UN-balanced" (balanced? "(1]") #f)
+     (test-eqv? "'{}[1' is UN-balanced" (balanced? "{}[1") #f)
 
      ))
 
