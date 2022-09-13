3fc0d0f93aa39b179102a3a1811f4c3ec3c43e7f
czrpb <czrpb00@gmail.com>
2022-09-13 09:34:01 -0700

    updates

--- exercism/matching-brackets/matching-brackets.rkt
+++ exercism/matching-brackets/matching-brackets.rkt
@@ -1,29 +1,24 @@
 #lang racket
 
+(require threading)
+
 (provide balanced?)
 
-(define open-brackets (set "(" "[" "{"))
-(define close-brackets (set ")" "]" "}"))
-(define is-open-bracket? (lambda (c) (set-member? open-brackets c)))
-(define is-bracket? (lambda (c) (or (is-open-bracket? c) (set-member? close-brackets c))))
+(define brackets "()[]{}")
+(define bracket-set (apply set (string->list brackets)))
+(define is-bracket? (lambda (c) (set-member? bracket-set c)))
+(define process (lambda (c a)
+                 (match* (c a)
+                   [(#\) (cons #\( t)) t]
+                   [(#\] (cons #\[ t)) t]
+                   [(#\} (cons #\{ t)) t]
+                   [(c a) (cons c a)]
+                )))
 
 (define (balanced? str)
-  (let ([closed-correctly? (match-lambda**
-                            [("(" ")") #t]
-                            [("[" "]") #t]
-                            [("{" "}") #t]
-                            [(_ _) #f]
-                            )]
-        )
-    (equal? '()
-            (foldl
-             (match-lambda**
-              [(c a) #:when (is-open-bracket? c) (cons c a)]
-              [(_ (or '() '(error))) '(error)]
-              [(c a) #:when (closed-correctly? (car a) c) (cdr a)]
-              [(c a) #:when ((negate closed-correctly?) c (car a)) '(error)]
-              [(_ a) a]
-              )
-             '() (filter is-bracket? (map ~a (string->list str)))
-             )
-            )))
+  (equal? '()
+          (~> str
+              (string->list _)
+              (filter is-bracket? _)
+              (foldl process '() _)
+              )))
