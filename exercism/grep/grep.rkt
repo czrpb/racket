7965f0ae7e399b0f0cfe5979d0bcb4cdb25c3a7a
czrpb <czrpb00@gmail.com>
2022-09-13 20:02:39 -0700

    updates

--- exercism/grep/grep.rkt
+++ exercism/grep/grep.rkt
@@ -1,30 +1,35 @@
 #lang racket
 
-(require threading)
-
 (provide grep)
 
-(define (grep flags str file )
-  (let* ([join-with-lf (lambda (l) (string-join l "\n"))]
-         [format-result (compose join-with-lf reverse)]
-         [nats (in-naturals)]
-         [lines (in-lines (open-input-file file #:mode 'text))]
+(define (grep flags str files)
+  (let* ([nats (in-naturals 1)]
+         [lines (lambda (file) (in-lines (open-input-file file #:mode 'text)))]
+         [str-match-fn (if (member "-x" flags) equal? string-contains?)]
          [matches? (compose
-                    (if (member "-v" flags) (negate string-contains?) string-contains?)
+                    (if (member "-v" flags) (negate str-match-fn) str-match-fn)
                     (if (member "-i" flags)
                         (lambda (line str) (values (string-downcase line) (string-downcase str)))
                         (lambda (line str) (values line str)))
                     )]
          )
-    (format-result
-     (for/fold ([result '()]) ([nat nats] [line lines])
-       (if (matches? line str)
-           (let* ([line (if (member "-n" flags) (~a nat ":" line) line)]
-                  )
-             (cons line result))
-           result)
-       ))
-
-    ))
-
-(grep '("-n" "-i") "and" "iliad.txt")
+    (flatten
+     (map (lambda (file)
+            (reverse
+             (for/fold ([result '()]) ([nat nats] [line (lines file)])
+               (if (matches? line str)
+                   (let* ([update-line-flags
+                           (compose
+                            (lambda (line)
+                              [if (and (member "-l" flags) (or (empty? result) (not (equal? file (car result)))))
+                                  file line])
+                            (lambda (line)
+                              [if (> (length files) 1) (~a file ":" line) line])
+                            (lambda (line)
+                              [if (member "-n" flags) (~a nat ":" line) line])
+                            )])
+                     (cons (update-line-flags line) result))
+                   result)
+               )))
+          files)
+     )))
