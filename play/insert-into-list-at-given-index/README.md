09fcd86c784f2a211dde4f8a17f39735c7cf58e4
czrpb <czrpb00@gmail.com>
2022-09-14 19:38:11 -0700

    updates

---
+++ play/insert-into-list-at-given-index/README.md
@@ -0,0 +1,9 @@
+# Insert items into a List Given an Index
+
+The inspiration of this problem came from a post on the Elixir forum:
+
+https://elixirforum.com/t/enum-over-list-and-insert-items-from-another-list-at-certain-indexes/50263
+
+## Indexes are Relative to the Given List
+
+## Indexes are Relative to the Resultant List
--- play/insert-into-list-at-given-index/insert-into-list.rkt
+++ play/insert-into-list-at-given-index/insert-into-list.rkt
@@ -2,13 +2,13 @@
 
 (require racket/hash)
 
-(provide insert-new-index insert-before-index)
+(provide insert-after-index insert-before-index)
 
-(define (insert-new-index list new-items)
-  (let (
-        [list-hash (apply hash (flatten (map cons (range 0 (length list)) list) ))]
-        [new-items-hash (apply hash (flatten (map reverse new-items)))]
-        )
+(define input1 '(("A" "C" "E") (("B" 1) ("D" 3))))
+(define input2 '((1 2 3 4 5 6 7 8 9 10) (("A" 1) ("B" 3) ("C" 6))))
+
+(define (insert-after-index list new-items)
+  (let ([new-items-hash (apply hash (flatten (map reverse new-items)))])
     (for/fold
      ([result '()] [list list] #:result (reverse result))
      ([i (range (+ (length list) (length new-items)))])
@@ -18,8 +18,14 @@
           ))
     ))
 
-(insert-new-index '("A" "C" "E") '(("B" 1) ("D" 3)))
-(insert-new-index '(1 2 3 4 5 6 7 8 9 10) '(("A" 1) ("B" 3) ("C" 6)))
+input1
+(printf "   --> ")
+(apply insert-after-index input1)
+
+input2
+(printf "   --> ")
+(apply insert-after-index input2)
+
 (printf "\n")
 
 (define (insert-before-index list new-items)
@@ -28,12 +34,17 @@
         ;[new-items (sort (map reverse new-items) #:key cadr string<?)]
         )
     (flatten
-     (for/list ([i (range 0 (length list) )] [l list])
+     (for/list ([i (range 0 (length list))] [l list])
        (if (hash-has-key? new-items-hash i)
            (cons (hash-ref new-items-hash i) l)
            l
            )))
     ))
 
-(insert-before-index '("A" "C" "E") '(("B" 1) ("D" 3)))
-(insert-before-index '(1 2 3 4 5 6 7 8 9 10) '(("A" 1) ("B" 3) ("C" 6)))
+input1
+(printf "   --> ")
+(apply insert-before-index input1)
+
+input2
+(printf "   --> ")
+(apply insert-before-index input2)
