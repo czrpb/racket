96fdc61b6c389756c652becbdfef03db32f9ac7d
czrpb <czrpb00@gmail.com>
2022-12-06 19:20:21 -0800

    day4 done

--- play/aoc/2022/day4.rkt
+++ play/aoc/2022/day4.rkt
@@ -1,15 +1,17 @@
 #lang at-exp racket
 
+(require relation/function)
+
 (require "read-input.rkt")
 
 (define test-lines
   (string-split
    @~a{2-4,6-8
-2-3,4-5
-5-7,7-9
-2-8,3-7
-6-6,4-6
-2-6,4-8}
+ 2-3,4-5
+ 5-7,7-9
+ 2-8,3-7
+ 6-6,4-6
+ 2-6,4-8}
    "\n")
   )
 
@@ -17,10 +19,59 @@
   (displayln "starting ...")
   (let* [(lines-fh (read-input "day4-input.txt"))
          (lines (car lines-fh))
+         ;(lines test-lines)
          (fh (cdr lines-fh))
+
+         ; helpers for sections
+         (strings->numbers (partial map string->number))
+         (make-section (λ (low-high) [range (car low-high) (add1 (cadr low-high))]))
+         (strings->sections (compose make-section strings->numbers))
+
+         ; making sections
+         (make-section (λ (section)
+                         (strings->sections (string-split section "-"))))
+         (make-sections (λ (line)
+                          (map make-section (string-split line ","))))
+         (sections->sets (partial map (partial apply set)))
+
+         (sets-subset? (λ (sets)
+                         (or
+                          (apply subset? sets)
+                          (apply subset? (reverse sets))
+                          )))
+
+         (sets-intersect? (λ (sets)
+                            ;(displayln (apply set-intersect sets))
+                            (not (set-empty? (apply set-intersect sets)))
+                            ))
+
          ]
 
-    'na
+    (displayln
+     (for/sum [(line lines)]
+       (let* [(sections (make-sections line))
+              (sets (sections->sets sections))
+              (subset? (sets-subset? sets))
+              ]
+         (displayln line)
+         (displayln sections)
+         (displayln sets)
+         (displayln subset?)
+         (if subset? 1 0)
+         ))
+     )
+
+    (displayln
+     (for/sum [(line lines)]
+       (let* [(sections (make-sections line))
+              (sets (sections->sets sections))
+              (intersect? (sets-intersect? sets))
+              ]
+         (displayln sets)
+         (displayln intersect?)
+         (if intersect? 1 0)
+         ))
+     )
 
     (close-input-port fh)
     )
