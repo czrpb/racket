4bc70d8abb6556bd7460244113636cee0f600baf
czrpb <czrpb00@gmail.com>
2023-02-04 13:25:43 -0800

    updates

--- play/gh-client/gh-client.rkt
+++ play/gh-client/gh-client.rkt
@@ -3,11 +3,13 @@
 (require net/url)
 (require json)
 
+; Parameters
 (define user (make-parameter #f))
 (define repo (make-parameter #f))
 (define pr (make-parameter #f))
 (define token (make-parameter #f))
 
+; URL request helpers
 (define (make-url-gh/pr user repo pr)
   (string->url (format "https://api.github.com/repos/~a/~a/pulls/~a" user repo pr))
   )
@@ -16,6 +18,7 @@
   (~a "Authorization: token " token)
   )
 
+; parse command-line
 (define action
   (command-line
    #:once-each
@@ -25,6 +28,21 @@
    [("-t" "--token") t "Token to access repo" (token t)]
    ))
 
+; PR fields we care about
+(define field-regexs
+  (hash
+   "ref" (regexp "^(FOO-\\d+)")
+   "title" (regexp "^(FOO-\\d+): .+$")
+   "body" (regexp "# SECTION 1\\r\\n# SECTION 2")
+   ))
+
+(define (response->fields resp)
+  (values
+   (hash-ref resp 'ref)
+   (hash-ref resp 'title)
+   (hash-ref resp 'body)
+   ))
+
 (let*-values
     [
      ((url-gh/pr) (make-url-gh/pr (user) (repo) (pr)))
@@ -32,9 +50,12 @@
       (http-sendrecv/url url-gh/pr #:headers (list (make-header/token (token))))
       )
      ((resp) (read-json port))
+    ;  ((ref title body) (response->fields resp))
+    ;  ((ref-valid) (regexp-match (hash-ref field-regexs ref)))
      ]
   (displayln (url->string url-gh/pr))
   (displayln status-line)
   (displayln headers)
   (pretty-display resp)
+  ; (displayln (~a "\n" ref " : " ref-valid))
   )
