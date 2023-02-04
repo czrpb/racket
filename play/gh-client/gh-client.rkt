016471d668d05b3d13d68e0693d3a07d7dca726e
czrpb <czrpb00@gmail.com>
2023-02-04 12:17:54 -0800

    updates

--- play/gh-client/gh-client.rkt
+++ play/gh-client/gh-client.rkt
@@ -1,20 +1,34 @@
 #lang racket
 
 (require net/url)
+(require json)
 
 (define user (make-parameter #f))
 (define repo (make-parameter #f))
+(define pr (make-parameter #f))
+(define token "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")
+(define header-token (~a "Authorization: token " token))
 
-(define url-pr-format "https://github.com/repos/~a/~a/pulls")
+(define (make-url-gh/pr user repo pr)
+  (string->url (format "https://api.github.com/repos/~a/~a/pulls/~a" user repo pr))
+  )
 
 (define action
   (command-line
    #:once-each
    [("-u" "--user") u "User that owns the repo" (user u)]
    [("-r" "--repo") r "Repo to get PRs for" (repo r)]
+   [("-n" "--pr") n "PR to get" (pr n)]
    ))
 
-(let
-    [(url-pr (format url-pr-format (user) (repo)))]
-  (displayln url-pr)
+(let*-values
+    [
+     ((url-gh/pr) (make-url-gh/pr (user) (repo) (pr)))
+     ((status-line headers port) (http-sendrecv/url url-gh/pr #:headers (list header-token)))
+     ((resp) (read-json port))
+     ]
+  (displayln (url->string url-gh/pr))
+  (displayln status-line)
+  (displayln headers)
+  (displayln resp)
   )
