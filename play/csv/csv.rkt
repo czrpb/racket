7586e2d5c2c99ae5a6a3f469c753a706c984abfd
czrpb <czrpb00@gmail.com>
2023-02-07 17:12:45 -0800

    updates

--- play/csv/csv.rkt
+++ play/csv/csv.rkt
@@ -9,7 +9,7 @@
 (define action
   (command-line
    #:once-each
-   [("-f" "--cvs-file") c "CVS filename" (csv-file c)]
+   [("-f" "--csv-file") c "CSV filename" (csv-file c)]
    ))
 
 (define (basic-auth-header user pass)
@@ -17,11 +17,23 @@
           (base64-encode (string->bytes/latin-1 (~a user ":" pass))))
   )
 
-(let* [
-       (csv-fh (open-input-file (csv-file) #:mode 'text))
-       (csv-lines (for/list [(line (in-lines csv-fh))] (string-split line #px"\\s*,\\s*")))
-       ]
-  (for/list [(line csv-lines)]
-    (displayln line)
+(define (csv-file-read csv-file field-sep)
+  (let* [
+         (csv-line-split (λ (line) (regexp-split field-sep (string-trim line "\""))))
+
+         (csv-fh (open-input-file csv-file #:mode 'text))
+         (header-line (csv-line-split (read-line csv-fh)))
+         (header-line-len (length header-line))
+         ]
+    (for/list [(line (in-lines csv-fh))]
+      (let [(fields (csv-line-split line))]
+        (if [(negate =) header-line-len (length fields)]
+            (raise "Failed to split record correctly; wrong number of resultant fields.")
+            fields
+            )
+        )
+      )
     )
   )
+
+(csv-file-read (csv-file) #px"\\s*\",\"\\s*")
