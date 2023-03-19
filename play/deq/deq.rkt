8044e6e9549259e93563b20eceaea827c4fc094d
Quentin Crain <czrpb00@gmail.com>
2023-03-19 10:28:43 -0700

    init

---
+++ play/deq/deq.rkt
@@ -0,0 +1,36 @@
+#lang racket
+
+
+(define (arrive q v)
+  (let [(front (car q)) (back (cadr q))]
+    (list front (cons v back))
+    )
+  )
+
+(define (front q)
+  (let [(front (car q)) (back (cadr q))]
+    (if (empty? front)
+        (car (reverse back))
+        (car front)
+        )
+    )
+  )
+
+(define (depart q)
+  (let [(front (car q)) (back (cadr q))]
+    (if (empty? front)
+        (let-values [((back front) (split-at back (quotient (length back) 2)))]
+          (list (cdr (reverse front)) back)
+          )
+        (list (cdr front) back)
+        )
+    )
+  )
+
+(let loop [(c 10) (i 7) (q '((1 2 3) (6 5 4)))]
+  (displayln q)
+  (if (zero? c)
+      q
+      (loop (sub1 c) (add1 i) (if (zero? (random 0 2)) (arrive q i) (depart q)))
+      )
+  )
