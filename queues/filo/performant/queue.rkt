a408568a5e81b0ff0ee7bb0d19c314704fea3db6
Quentin Crain <czrpb00@gmail.com>
2023-03-25 12:15:33 -0700

    implemented

--- queues/filo/performant/queue.rkt
+++ queues/filo/performant/queue.rkt
@@ -2,6 +2,8 @@
 
 (define (queue) '())
 
+(define (empty q) (equal? '() q))
+
 (define (add e q) (cons e q))
 
 (define (next q) (car q))
@@ -15,3 +17,15 @@
 (define q (foldl add (queue) input))
 
 (pretty-print q)
+
+(define all (reverse
+             (let loop [(q q) (a '())]
+               (if (empty q)
+                   a
+                   (loop (remove q) (cons (next q) a))
+                   )
+               )
+             )
+  )
+
+(pretty-print all)
