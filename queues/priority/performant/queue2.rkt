1c9b914b259e6c423e1e592ee6eb095cde71f868
Quentin Crain <czrpb00@gmail.com>
2023-03-28 07:52:05 -0700

    updates

--- queues/priority/performant/queue2.rkt
+++ queues/priority/performant/queue2.rkt
@@ -1,29 +1,19 @@
 #lang racket
 
 ; #####
-; ##     This queue is implemented using a pairing heap
+; ##     This queue is implemented using a binary tree
 ; #####
 
 (define (queue) '())
 
 (define (empty q) (equal? '() q))
 
-(define (element key data (children '())) (list key data children))
+(define (element key data (left '()) (right '())) (list key data left right))
 (define (element-key e) (first e))
 (define (element-data e) (second e))
-(define (element-children e) (third e))
+(define (element-left e) (third e))
+(define (element-right e) (fourth e))
 
-(define (meld q1 q2)
-  (cond
-    [(empty q1) q2]
-    [(empty q2) q1]
-    [(< (element-key q1) (element-key q2))
-     (element (element-key q1) (element-data q1) (cons q2 (element-children q1)))]
-    [else
-     (element (element-key q2) (element-data q2) (cons q1 (element-children q2)))
-     ]
-    )
-  )
 
 (define (add k-d q) (meld (element (first k-d) (second k-d)) q))
 
