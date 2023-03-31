ada12c878fd323cf92c4844c2a4dd5f058db24d3
Quentin Crain <czrpb00@gmail.com>
2023-03-31 07:25:48 -0700

    updates

---
+++ queues/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Priority Queue: List Implementation"
+(time racket priority/performant/queue-list.rkt) >/tmp/priq-list.out 2>&1
+
+echo "Priority Queue: Sorted List Implementation"
+(time racket priority/performant/queue-sortedlist.rkt) >/tmp/priq-sortedlist.out 2>&1
+
+echo "Priority Queue: Binary Tree Implementation"
+(time racket priority/performant/queue-binarytree.rkt) >/tmp/priq-btree.out 2>&1
+
+echo "Priority Queue: Pairing Heap Implementation"
+(time racket priority/performant/queue-pairingheap.rkt) >/tmp/priq-pairingheap.out 2>&1
