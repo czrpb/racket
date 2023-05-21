8f208624b2a8d0b664a84863f6b5604e877991ea
Quentin Crain <czrpb00@gmail.com>
2023-05-20 17:03:59 -0700

    updates

--- play/log/log.rkt
+++ play/log/log.rkt
@@ -3,6 +3,13 @@
 (displayln "\nStarting ....\n")
 
 ; See
+;   https://www.brics.dk/RS/04/17/BRICS-RS-04-17.pdf
+; Summary
+;    log10 M = d0 + d1/10 + d2/10^2 + d3/10^3 + ...
+;    M       = 10^(d0 + d1/10 + d2/10^2 + d3/10^3 + ...)
+;    M       = 10^d0 * 10^(d1/10 + d2/10^2 + d3/10^3 + ...)
+;    M/10^d0 = 10^(d1/10 + d2/10^2 + d3/10^3 + ...)
+;    (M/10^d0)^10 = 10^(d1 + d2/10^1 + d3/10^2 + ...)
 
 (define num
   (match (current-command-line-arguments)
