fed212c20b8d0bca4ec5e669332e94aeee85ba0b
Quentin Crain <czrpb00@gmail.com>
2023-05-24 11:43:49 -0700

    updates

--- play/entropy-information/plot.rkt
+++ play/entropy-information/plot.rkt
@@ -4,11 +4,20 @@
 (plot-new-window? #t)
 
 (define (log2 v) (log v 2))
+(define (log10 v) (log v 10))
+
 (define (entropy pct) (* -1 pct (log2 pct)))
 
-(plot-x-label "Pct")
+(plot-x-label "Percent / Frequency")
 (plot-y-label "Entropy")
-(plot (function entropy 0 1
-                #:y-max 0.532
-                #:label "Entropy: (* -1 pct (log2 pct))"
-                ))
+(plot (append
+       (list (function entropy 0 1
+                       #:y-max 0.532
+                       ;#:y-max 0.16
+                       ;#:y-max 0.37
+                       #:label "Entropy: (* -1 pct (log2 pct))"
+                       ))
+       (map (λ (pct) (point-label (vector pct (entropy pct))))
+            (range 0.025 1 0.025))
+       )
+      )
