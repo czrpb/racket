83f68806532d7608c77505b66ccfc769f5e22ca3
Quentin Crain <czrpb00@gmail.com>
2023-11-09 08:09:23 -0800

    updates: 20231109-080923

--- play/rod_cutoff/rod_cutoff.rkt
+++ play/rod_cutoff/rod_cutoff.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
 (define l1 '(1 1 3 4))
+(define l2 '(1 1 7 2 2 4 4 4 4 3 11 11 11 11 11 19 1 5 7 7 7 3 4))
 
 (define [rc l acc]
   [cond
@@ -14,6 +15,8 @@
             [l-new (filter-not min? l)]
             [l-updated (map -min l-new)]
             )
+       (writeln l)
+       (writeln acc)
        (rc l-updated [cons l-len acc])
        ]
      )
@@ -21,4 +24,5 @@
   )
 
 (printf "~a -> ~a\n" l1 [rc (sort l1 <) '()])
+(printf "~a -> ~a\n" l2 [rc (sort l2 <) '()])
 
